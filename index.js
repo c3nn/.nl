0f0b294e31b2a4f78acbae1f0b56b3c535f6fa4d
Conmann <hello@c3n.nl>
2023-12-17 11:51:19 -0600

    small update for mobile testing

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { $, css } from "/lib/c3nnUtil.js";
+import { $, $all, css } from "/lib/c3nnUtil.js";
 
 if(css('--SupportsCSSHasSelector') != "true"){
 	$('#enableJS').innerHTML = '( please use a chromium based browser for the best experience or a firefox version >120 )';
@@ -15,3 +15,16 @@ window.addEventListener('scroll', (e) => {
 // 🥚
 let emailUsernamesEggList = ['c','conmann','Superuser','Nobody','Chucklenuts','Punslinger','uninst','onlyCatMemes','hello','conmandev','goofly'];
 $('.atSymbol').title = $('.atSymbol').title.replace('[Please Enable Javascript]',emailUsernamesEggList[Math.floor(Math.random()*emailUsernamesEggList.length)]);
+
+$all('.iframeProjShowcase .iframeCont .zoomCont .minus').forEach(element => {
+	element.addEventListener('click', (e) => {
+		let iframeCont = element.parentElement.parentElement;
+		iframeCont.css('--z', Math.min(iframeCont.css('--z',null,{num:true}) + 0.1, 10));
+	});
+});
+$all('.iframeProjShowcase .iframeCont .zoomCont .plus').forEach(element => {
+	element.addEventListener('click', (e) => {
+		let iframeCont = element.parentElement.parentElement;
+		iframeCont.css('--z', Math.max(iframeCont.css('--z',null,{num:true}) - 0.1, 0.2));
+	});
+});
